19db7d9190e04f237ae0a3539175a81e2a0d6217
Francisco Loureiro <64337888+f1904lou@users.noreply.github.com>
2023-07-05 19:26:14 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+from fastai.vision.all import *
+learn = load_learner('Photo_classification_model.pkl')
+
+labels = ["Moutain Landscape", "Beach Landscape", "Group Photo"]
+def predict(img):
+    img = PILImage.create(img)
+    pred,pred_idx,probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+title = "Travel Photos Classifier"
+description = "A travel photo classifier trained with fastai. Created as a demo for Gradio and HuggingFace Spaces."
+interpretation='default'
+enable_queue=True
+
+gr.Interface(fn=predict,inputs=gr.inputs.Image(shape=(512, 512)),outputs=gr.outputs.Label(num_top_classes=3),title=title,description=description,interpretation=interpretation,enable_queue=enable_queue).launch()
